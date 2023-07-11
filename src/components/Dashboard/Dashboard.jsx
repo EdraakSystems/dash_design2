a5518be8c33af07d2f47b9d0a9c64e3b87aa3b38
shahab-edraak <shahab@edraaksystems.com>
2023-07-11 17:00:48 +0500

    font-family change

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import './Dashboard.css'
 import { BsSearch } from 'react-icons/bs'
+import { BsChatText } from 'react-icons/bs'
+import { IoIosNotificationsOutline } from 'react-icons/io'
 
 const Dashboard = () => {
   return (
     <div className='dashboard'>
       <section className='dash_into'>
-        <h1>
-          School Mentor
-        </h1>
-        <div className="nav_search">
-          <input type="text" placeholder='Search' />
-          <button>
-            <BsSearch />
-          </button>
+        <div className='heading'>
+          <span class="school">School</span>
+          <span class="mentor">Mentor</span>
+        </div>
+        <div className="dash_intro_logo">
+          <div className="nav_search">
+            <input type="text" placeholder='Search' />
+            <button>
+              <BsSearch />
+            </button>
+          </div>
+          <button className='border'><BsChatText /></button>
+          <button className='border'><IoIosNotificationsOutline /></button>
         </div>
       </section>
       <section className='dash_cards1'></section>
