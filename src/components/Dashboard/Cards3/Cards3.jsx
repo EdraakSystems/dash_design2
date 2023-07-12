6110a2088f0d7da63a00dfb6500d1489ff093480
shahab-edraak <shahab@edraaksystems.com>
2023-07-12 18:28:21 +0500

    table

--- src/components/Dashboard/Cards3/Cards3.jsx
+++ src/components/Dashboard/Cards3/Cards3.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Cards3.css'
+import Table from './Table'
 
 const Cards3 = () => {
     return (
         <section className='dash_cards3'>
+            <div className="cards3_table cards2_width">
+                <Table />
+            </div>
+            <div className="cards3_graphs cards2_width">
 
+            </div>
+            <div className="cards3_cards cards2_width">
+
+            </div>
         </section>
     )
 }
