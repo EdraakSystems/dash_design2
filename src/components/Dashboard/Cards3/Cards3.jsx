d5a0124b13ed704c97cafea38fca4a07dd543c00
shahab-edraak <shahab@edraaksystems.com>
2023-07-14 12:13:27 +0500

    cards3 structure

--- src/components/Dashboard/Cards3/Cards3.jsx
+++ src/components/Dashboard/Cards3/Cards3.jsx
@@ -3,6 +3,7 @@ import "./Cards3.css";
 import Table from "../Table/Table";
 import Barchart from "../Charts/Barchart";
 import Areachart2 from "../Charts/Areachart2";
+import { AiOutlineDown } from "react-icons/ai";
 
 const Cards3 = () => {
   return (
@@ -18,7 +19,47 @@ const Cards3 = () => {
           <Areachart2 />
         </div>
       </div>
-      <div className="cards3_cards cards2_width"></div>
+      <div className="cards3_cards cards2_width">
+        <div className="cards3_cards_card">
+          <div className="card_heading">
+            <h3>Birthdays</h3>
+            <button>
+              <AiOutlineDown />
+            </button>
+          </div>
+          <div className="card_data">
+            <div className="bday_info">
+              <h4>Teacher</h4>
+              <div>
+                <img
+                  src="https://mdbootstrap.com/img/new/avatars/7.jpg"
+                  alt=""
+                />
+                <p>Nic John</p>
+              </div>
+            </div>
+            <div className="bday_info">
+              <h4>Student</h4>
+              <div>
+                <img
+                  src="https://mdbootstrap.com/img/new/avatars/6.jpg"
+                  alt=""
+                />
+                <p>John Doe</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="cards3_cards_card">
+          <div>
+            <h3>Updates</h3>
+            <button>
+              <AiOutlineDown />
+            </button>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 };
